ec26c705d3707f0bc5aba63aa9b959516377a818
BitterKanegul <15769643+BitterKanegul@users.noreply.github.com>
2025-07-09 16:00:55 -0400

    start uf impl

---
+++ union-find.py
@@ -0,0 +1,20 @@
+# An implementation of union find data structure and different test cases.
+# 1. Using Python Classes to represent nodes.
+# 2. Looking at existing implementations.
+# 3. Large number of testcases
+#       - Randomly generate input array and sequence of unions among them. Upto a million
+#       - Correctness testcases, idempotence, reflexivity, disjointness
+#           - exhaustive
+#           - randomized
+
+# What must a union-find data structure do?
+# A Map from elements to representatives
+#   - Each element maps to one representative.
+#   - Every element is unique i.e. there are no two elements with the same id.
+#   - Every element maps to one representative of the disjoint set it is a part of.
+#   We have the following operations on a Disjoint Set structure:
+#       - Find: Given an input element, find the canonical representative of the set it is a part of
+#       - Merge: Given two elements, merge the sets that they are a part of.
+#       - Add: Add a new element to the Map(by default it maps to itself)
+
+import numpy
